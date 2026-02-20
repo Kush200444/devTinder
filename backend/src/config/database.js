4d15c30c999f932ca6e399cf9d0d63b6754fedb3
Kush200444 <kush62831@gmail.com>
2026-02-21 05:21:50 +0530

    Delete database.js

--- backend/src/config/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const User = require("../models/user");
-const connectDB = async() => {   
- await mongoose.connect("mongodb+srv://kush62831:GIl8qXLdPRgvbAhz@cluster0.ejgco.mongodb.net/devTinder");
-}; 
-module.exports = {connectDB};
