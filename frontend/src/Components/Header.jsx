3756f8d8eefb5e36ebbec815b127e5a84804e6b7
Kush <kush62831@gmail.com>
2026-04-11 22:36:59 +0530

    readme updated

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   <div className="flex gap-2">
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full -mx-100">
+        <div className="w-10 rounded-full mx-20">
           <img
             alt="Tailwind CSS Navbar component"
             src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
