90ae2fe17e8ce6a6c60645331c792fb1653ceb1f
Kush <kush62831@gmail.com>
2026-04-11 22:30:30 +0530

    Header created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./Components/Header";
+
 const App = () => {
-   return(
+   return(<div>
+    <Header/>
     <div>
         <h1 class="text-3xl font-bold underline">
     Hello world!</h1>
     </div>
+  </div>
    )
 }
 
