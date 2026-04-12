5832ef420e8eb78cc2bca649d26a67393b61fb7b
Kush <kush62831@gmail.com>
2026-04-13 01:46:51 +0530

    created appStore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
 import Profile from "./Components/Profile";
 import Body from "./Components/Body";
+import { Provider } from "react-redux";
 
 
 const App = () => {
   return (
+    <Provider store={appStore}>
     <Router basename="/">
-
       <Routes>
         <Route path="/" element={<Body />}>
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes>
-
     </Router>
+    </Provider>
   );
 }
 
